8175d11ec2b37702bce9fdd96a04751ad07c7a51
PreetiBoddeti <iteerp999@gmail.com>
2020-09-11 01:12:27 +0530

    changed swaggerDocument within app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const swaggerUi = require('swagger-ui-express');
 const YAML = require('yamljs');
 const swaggerDocument = YAML.load('./swagger.yaml');
  
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggeDocument));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 //port
 const port = process.env.PORT 
