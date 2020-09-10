43d67898f298c62d47d1df4bddcf8f4f1001f2b3
PreetiBoddeti <iteerp999@gmail.com>
2020-09-10 22:22:02 +0530

    small change to check the status

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const YAML = require('yamljs');
 const swaggerDocument = YAML.load('./swagger.yaml');
  
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggeDocument));
+
+//port
 const port = process.env.PORT 
 app.listen(port, () => {
     console.log("Server is up and running on port 3000");
