1e70839eff56217bd5fbc39ce4fc8ea5e0940e01
PreetiBoddeti <iteerp999@gmail.com>
2020-09-10 21:55:18 +0530

    removed irrelavant folders

---
+++ app.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+const express = require('express');
+const app = express();
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
+ 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggeDocument));
+const port = process.env.PORT 
+app.listen(port, () => {
+    console.log("Server is up and running on port 3000");
+});
